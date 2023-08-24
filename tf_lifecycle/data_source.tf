5ba133e07ce36be60285a5f397a411bf2823d2d5
sathishkumarsankarn <41965179+sathishkumarsankarn@users.noreply.github.com>
2023-08-24 14:51:18 +0530

    Update data_source.tf

--- tf_lifecycle/data_source.tf
+++ tf_lifecycle/data_source.tf
@@ -1,6 +1,6 @@
 resource "local_file" "hi" {
   filename = var.filename
-  content = random_string.hello.id
+  content = data.local_file.god.content
 }
 
 resource "random_string" "hello" {
@@ -10,6 +10,7 @@ resource "random_string" "hello" {
   }
 }
 
+# god.txt file is not created by terraform but using data (datasource) we are getting the content of god.txt and use it in our terraform configuration
 data "local_file" "god" {
   filename = "/root/god.txt"
 }
