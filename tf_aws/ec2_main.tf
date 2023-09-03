260ce29f99e7d2ce323f412996513d98ad19b27a
sathishkumarsankarn <41965179+sathishkumarsankarn@users.noreply.github.com>
2023-09-03 17:37:19 +0530

    Update ec2_main.tf

--- tf_aws/ec2_main.tf
+++ tf_aws/ec2_main.tf
@@ -1,4 +1,8 @@
 resource "aws_instance" "webserver" {
   ami = "ami-0823687123765"
   instance_type = "t2.micro"
+  tags = {
+    Name = "webserver instances"
+    description = "It is having Nginx Webserver"
+  }
 }
